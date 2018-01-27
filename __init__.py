6c9bd129dc48a81f2525f93007f2c8ad969b6569
Marco Zangari <mjzangari@gmail.com>
2018-01-26 19:35:19 -0800

    added packages

--- __init__.py
+++ __init__.py
@@ -1,13 +1,16 @@
 """."""
 
-
+from random import randint as rint
 citations = ['A party without cake is just a meeting.',
                 'I was 32 when I started cooking; up until then, I just ate.',
                 'The only real stumbling block is fear of failure. In cooking you\'ve got to have a what-the-hell attitude.']
 
 
-def rand_cite(c):
+def rand_cite(c=citations):
     """Return random citation from list of citations."""
     number = (len(c) - 1)
     rand = rint(0, number)
     print(c[rand])
+
+if __name__ == '__main__':
+    rand_cite()
